d7011dce8b3e1d74ccaf07e4dd869a098937fca0
huki <gk7huki@gmail.com>
2016-06-19 15:06:32 +0530

    added login form

--- login.php
+++ login.php
@@ -1,7 +1,12 @@
 <?php include 'header.php'; ?>
 
 <div id="login_text">
-<p><i>You are viewing this page as a Guest.</i></p>
+<form action="index.php" method="post">
+<p><i>You are viewing this page as a Guest.</i></p><br>
+<p>Username:<br><input type="text" name="username"></p>
+<p>Password:<br><input type="password" name="password"></p><br>
+<p><input class="button_login" type="submit" value="Login" name="login"></p>
+</form>
 </div>
 
 <?php include 'footer.php'; ?>
